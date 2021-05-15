c41a6fe433106a2ae095a2f0aa305f7b690d44ad
Daria1325 <55806465+Daria1325@users.noreply.github.com>
2021-05-15 20:32:51 +0300

    Add files via upload

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+
+(function(){
+var byeSpeaker ={
+	speak: function (name) {
+  console.log(speakWord + " " + name);
+}
+};
+
+var speakWord = "Good Bye";
+window.byeSpeaker = byeSpeaker;
+})();
+
+
